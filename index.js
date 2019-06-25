a10944d96b98d5cef5df9273e4a7c024db9416f6
Ronson Wuniardy <rwuniard@hotmail.com>
2019-06-24 22:28:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var redis = require('redis');
+
+var port = 6379;
+var host = "127.0.0.1";
+var client = redis.createClient(port, host);
+
+client.on('connect', ()=> {
+    console.log('Redis client connected');
+});
+
+client.on('error', (err) => {
+    console.log('Something went wrong' + err);
+});
+
+client.set('my test key', 'my test value', redis.print);
+client.get('my test key', (error, result) => {
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+    console.log('GET result ->' + result);
+});
+
+// client.get('myid', (error, result) => {
+//     if (error) {
+//         console.log(error);
+//         throw error;
+//     }
+//     console.log('GET result ->' + result);
+// });
